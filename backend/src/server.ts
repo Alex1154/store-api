872738ddd4385d4394b936f9a94192c199ae4f6d
João Alexandre <aleramosfilho@gmail.com>
2022-03-27 00:38:47 -0300

    gitflow-hotfix-stash: fixing-imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
-import express from "express"
-import cors from "cors"
-require("dotenv").config({ path: ".env" })
+import express from "express";
+import cors from "cors";
+require("dotenv").config({ path: ".env" });
+import { routes } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(routes);
+app.use(express.json());
+app.use(cors());
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
