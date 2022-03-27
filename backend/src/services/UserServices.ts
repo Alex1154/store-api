d34fe6146ad6f866671398854c52c2a4fefca568
João Alexandre <aleramosfilho@gmail.com>
2022-03-27 01:36:51 -0300

    Creating createUser service and adding to post router on UserController

--- backend/src/services/UserServices.ts
+++ backend/src/services/UserServices.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 export class UserService {
   public async getUsers(req: Request, res: Response) {
     const users = await prismaClient.user.findMany();
-    res.json(users);
+    return res.json(users);
   }
 
   public async getUser(req: Request, res: Response) {
@@ -14,6 +14,17 @@ export class UserService {
         userId: id,
       },
     });
-    res.json(user);
+    return res.json(user);
+  }
+
+  public async createUser(req: Request, res: Response) {
+    const { name, email } = req.body;
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    return res.json(user);
   }
 }
