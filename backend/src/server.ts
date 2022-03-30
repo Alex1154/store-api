6f20d82b2f54e249be31fe55c68600070013fb1f
João Alexandre <aleramosfilho@gmail.com>
2022-03-30 04:29:21 -0300

    creating delete and put routers in usercontroller

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,10 +6,15 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(routes);
-app.use(express.json());
+
 app.use(cors());
 
 const port = process.env.PORT || 3000;
+var cleanExit = function () {
+  process.exit();
+};
+process.on("SIGINT", cleanExit); // catch ctrl-c
+process.on("SIGTERM", cleanExit); // catch kill
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- backend/src/services/UserServices.ts
+++ backend/src/services/UserServices.ts
@@ -9,22 +9,97 @@ export class UserService {
 
   public async getUser(req: Request, res: Response) {
     const { id } = req.params;
-    const user = await prismaClient.user.findUnique({
-      where: {
-        userId: id,
-      },
-    });
+    const user = await prismaClient.user
+      .findUnique({
+        where: {
+          userId: id,
+        },
+      })
+      .catch((err) => {
+        return res.json({
+          message: "User not found",
+          error: err,
+        });
+      });
     return res.json(user);
   }
 
   public async createUser(req: Request, res: Response) {
     const { name, email } = req.body;
-    const user = await prismaClient.user.create({
-      data: {
-        name,
-        email,
-      },
+    const user = await prismaClient.user
+      .create({
+        data: {
+          name,
+          email,
+        },
+      })
+      .then((user) => {
+        return res.json({
+          message: "User created",
+          user,
+        });
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          message: "cant create user",
+          error: err,
+        });
+      });
+  }
+  public async updateUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const user = await prismaClient.user
+      .update({
+        where: {
+          userId: id,
+        },
+        data: {
+          name,
+          email,
+        },
+      })
+      .then((user) => {
+        return res.json({
+          message: "User updated",
+          user,
+        });
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          message: "cant update user",
+          error: err,
+        });
+      });
+  }
+
+  public async deleteUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = await prismaClient.user
+      .delete({
+        where: {
+          userId: id,
+        },
+      })
+      .then((user) => {
+        return res.json({
+          message: "User deleted",
+          user,
+        });
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          message: "cant delete user",
+          error: err,
+        });
+      });
+  }
+
+  public async deleteUsers(req: Request, res: Response) {
+    const users = await prismaClient.user.deleteMany();
+    return res.json({
+      message: "Users deleted",
+      users,
     });
-    return res.json(user);
   }
 }
